0e52e96a0d4c259b873cdba37a13a92d710ea141
Darrell Ireland <darrell.ireland@tandf.co.uk>
2021-04-23 16:22:39 +0100

    gatsby-source-wordpress plugin added

--- gatsby-config.js
+++ gatsby-config.js
@@ -126,5 +126,11 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+       url: `https://diwptesting.wpengine.com/graphql`,
+      },
+    }
   ],
 }
